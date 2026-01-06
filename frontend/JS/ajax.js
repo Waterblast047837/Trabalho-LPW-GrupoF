ea1150f6cca6b8fdd169eb6d0a7bc415371eaaee
Mateus Pinheiro dos Santos <100387595+Waterblast047837@users.noreply.github.com>
2026-01-05 23:51:06 -0300

    Update redirect URLs to relative paths

--- frontend/JS/ajax.js
+++ frontend/JS/ajax.js
@@ -3,7 +3,7 @@ function verificarLogin() {
     
     if (!token) {
         console.warn('Usuário não autenticado, redirecionando para login');
-        window.location.href = 'http://127.0.0.1:5500/pagina-registro.html'; 
+        window.location.href = 'pagina-registro.html';
         return false;
     }
     return true;
@@ -14,7 +14,7 @@ function renovarToken() {
     
     if (!refreshToken) {
         console.warn('Refresh token não encontrado, redirecionando para login');
-        window.location.href = 'http://127.0.0.1:5500/pagina-registro.html';
+        window.location.href = 'pagina-registro.html';
         return false;
     }
     
@@ -36,7 +36,7 @@ function renovarToken() {
             console.warn('Refresh token expirou também, redirecionando para login');
             localStorage.removeItem('acess_token');
             localStorage.removeItem('refresh_token');
-            window.location.href = '/login.html';
+            window.location.href = 'pagina-registro.html';
             return false;
         }
     });
@@ -78,7 +78,7 @@ formCadastro.on('submit', function(f) {
                     localStorage.setItem('acess_token', resposta.acess_token)
                     localStorage.setItem('refresh_token', resposta.refresh_token)
 
-                    window.location.href = 'http://127.0.0.1:5500/index.html'
+                    window.location.href = 'index.html'
                 },
 
                 error: function(erro) {
@@ -111,7 +111,7 @@ formLogin.on('submit', function(f) {
             localStorage.setItem('acess_token', resposta.acess_token)
             localStorage.setItem('refresh_token', resposta.refresh_token)
 
-            window.location.href = 'http://127.0.0.1:5500/index.html'
+            window.location.href = 'index.html'
         },
 
         error: function(erro) {
